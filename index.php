29df4ffe4b44933b470291a2ac53dbcd409ef454
Jon Boyd <jboyd@aberdeenpublishing.com>
2015-06-10 12:05:05 -0700

    Implemented API functions to allow get requests for a list of articles from api/category/:term_id/:count/:index allowing the user of the api to grab a list of postID's for any category -> Does not fail gracefully

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require 'Slim/Slim/Slim.php';
 
 //Grabbing the DB credentials to acces the necessary SQL tables
 require 'config/config.php';
-var_dump($categories);
+//var_dump($categories);
 
 //Creates a new mysqli connection to database
 function dbConnect($host = DB_HOST, $user = DB_USER, $password = DB_PASSWORD, $dbname = DB_NAME) {
@@ -29,16 +29,96 @@ $app->get('/hello/:name', function ($name) use ($app){
 
 /*  News Article API
 *
-*   - Get list of recent articles from /articles/
-*   - Get specific article from /articles/:id
-*   - Get list of article categories from /articles/categories
-*   - Get list of articles from a specific category from /articles/categories/:category
+*   - Get a default list of recent articles from specific category -> /category/:term_id
+*   - Get list of articles from specific category -> /category/:term_id(categoryID)/:count(numOfPostsToReturn)/:index(lastPostID)
+*   - Get specific article (title/body/views/datePosted/author/images/gallery) -> /article/:id(postID)
+*   ? Get list of article categories from /articles/categories (May be usful in the future if we do subcategories)
 *   ? Get specific article by name from /articles/:name
 *   ? Get list of articles on a certain date from /articles/:date(D-M-Y)
-*   ?
 */
 
+function getRecentPostsByCategory($table_prefix, $term_id = 2, $count = 5, $index = 0) {
+    $mysqli = dbConnect();
 
+    //TODO: Remove else, set indexDate default to current date - format: '2015-01-23 14:33:02'
+    //TODO: Close all mysqli objects
+
+    $indexDate = new DateTime();
+    $indexDate = $indexDate->format( 'Y-m-d H:i:s');
+
+    if ($index !== 0) {
+        $indexDate = findIndexDate($index, $mysqli);
+    }
+
+    $sql = "SELECT SQL_CALC_FOUND_ROWS ez_posts.ID
+            FROM ez_posts INNER JOIN ez_term_relationships ON
+            (ez_posts.ID = ez_term_relationships.object_id) WHERE 1=1
+            AND ( ez_term_relationships.term_taxonomy_id IN ($term_id) )
+            AND ez_posts.post_type = 'post'
+            AND (ez_posts.post_status = 'publish'
+            OR ez_posts.post_status = 'expired'
+            OR ez_posts.post_status = 'private')
+            AND ez_posts.post_date < '$indexDate'
+            GROUP BY ez_posts.ID ORDER BY ez_posts.post_date
+            DESC LIMIT 0, $count";
+
+
+    if ($result = $mysqli->query($sql)) {
+        while($row = $result->fetch_object()) {
+            $resultArray[] = $row;
+        }
+    } else {
+        return 'Select Statement Failed';
+    }
+
+    return $resultArray;
+}
+
+function findIndexDate($index, $mysqli) {
+
+    $sql = "SELECT ez_posts.post_date FROM ez_posts WHERE ez_posts.ID = $index";
+
+    if ($result = $mysqli->query($sql)) {
+        while($row = $result->fetch_object()) {
+            $resultField = $row->post_date;
+        }
+    }
+    return($resultField);
+}
+
+function sendJSONResponse($app, $responseBody) {
+    // CREATE RESPONSE
+    $response = $app->response();
+    // Allow access to everyone for initial testing purposes
+    // TODO: Implemention authentication protocol so the API can only be accessed by the app
+    $response->header('Access-Control-Allow-Origin', '*');
+    $response->write(json_encode($responseBody));
+}
+
+$app->group('/category', function () use ($app) {
+
+    $app->get('/:term_id', function($term_id) use ($app) {
+        $resultArray = getRecentPostsByCategory(TABLE_PREFIX, $term_id);
+        sendJSONResponse($app, $resultArray);
+    });
+
+    $app->get ('/:term_id/:count', function($term_id,$count) use ($app) {
+        $resultArray = getRecentPostsByCategory(TABLE_PREFIX, $term_id, $count);
+        sendJSONResponse($app, $resultArray);
+    });
+
+    $app->get ('/:term_id/:count/:index', function($term_id, $count, $index) use ($app) {
+        $resultArray = getRecentPostsByCategory(TABLE_PREFIX, $term_id, $count, $index);
+        sendJSONResponse($app, $resultArray);
+    });
+});
+
+$app->get('/category/:term_id', function ($term_id) use ($app) {
+
+
+
+
+});
 
 
 // OLD CODE BELOW HERE
