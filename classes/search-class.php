f56be2968169f5ec34785bff2bb0fc1696b7f441
Alex Johnston <ajohnston@aberdeenpublishing.com>
2015-08-26 13:11:39 -0700

    Fixed Search Class - searching properly now

--- classes/search-class.php
+++ classes/search-class.php
@@ -7,6 +7,10 @@ class SearchAPI {
   public function searchPosts($table_prefix, $searchKey, $count = 10, $index = 0) {
       header('Access-Control-Allow-Origin', '*');
 
+      // parse search string:
+
+      $new_string = $this->cleanSearchString($searchKey);
+
       $mysqli = dbConnect();
       $indexDate = $this->setIndexDate($table_prefix, $index, $mysqli);
 
@@ -14,10 +18,11 @@ class SearchAPI {
       FROM ".$table_prefix."posts
       WHERE post_status = 'publish'
       AND ".$table_prefix."posts.post_date < '$indexDate'
-      AND post_title LIKE '%". $searchKey . "%'
+      AND post_title LIKE '". $new_string . "'
+      AND post_type = 'post'
       ORDER BY ".$table_prefix."posts.post_date
       DESC LIMIT 0, $count";
-    //  echo $sql;
+ //echo $sql;
     //  $mysqli->query($sql);
 
     //  $sql = "SELECT ID,post_author,post_date,post_content,post_title,comment_count FROM ".$table_prefix."posts WHERE `ID` = ($post_id)";
@@ -43,11 +48,17 @@ class SearchAPI {
   public function getSearchRecordCount($table_prefix, $searchKey, $search_result, $index = 0) {
     $mysqli = dbConnect();
     $indexDate = $this->setIndexDate($table_prefix, $index, $mysqli);
-    $sql = "SELECT count(*) as count 
+
+    $new_string = $this->cleanSearchString($searchKey);
+
+
+    $sql = "SELECT count(*) as count
     FROM ".$table_prefix."posts
     WHERE post_status = 'publish'
     AND ".$table_prefix."posts.post_date < '$indexDate'
-    AND post_title LIKE '%". $searchKey . "%'";
+    AND post_title LIKE '". $new_string . "'
+    AND post_type = 'post'";
+
 
     $result = $mysqli->query($sql);
 
@@ -80,6 +91,77 @@ class SearchAPI {
           }
       }
       return $resultField ;
+  }
+
+  private function cleanSearchString($search_string) {
+
+    // Strip HTML Tags
+    $clear = strip_tags($search_string);
+    // Clean up things like &amp;
+    $clear = html_entity_decode($clear);
+    // Replace single quote with Double Single Quote
+    $clear = str_replace("'", "''", $clear);
+    // Strip out any url-encoded stuff
+    $clear = urldecode($clear);
+
+    $remove_words = array(
+                        "kamloops",
+                        " of ",
+                        " a ",
+                        " in ",
+                        " it ",
+                        " is ",
+                        " the ",
+                        " for ",
+                        " he ",
+                        " she ",
+                        " him ",
+                        " her ",
+                        " on ",
+                        " be ",
+                        " to ",
+                        " had ",
+                        " has ",
+                        " and ",
+                        " or ",
+                        " if ",
+                        " have ",
+                        " all ",
+                        " its ",
+                        " an ",
+                        " but ",
+                        " into ",
+                        " no ",
+                        " not ",
+                        " such ",
+                        " that ",
+                        " their ",
+                        " then ",
+                        " there ",
+                        " these ",
+                        " they ",
+                        " this ",
+                        " was ",
+                        " will ",
+                        " with "
+                      );
+    // Replace non-AlNum characters with space
+    $clear = str_replace($remove_words, ' ', $clear);
+    //$clear = preg_replace('/[^A-Za-z0-9]/', ' ', $clear);
+    // Replace Multiple spaces with single space
+    $clear = preg_replace('/ +/', ' ', $clear);
+    // Trim the string of leading/trailing space
+    $clear = trim($clear);
+
+
+    $new_string = "%".$clear."%";
+
+    $new_string = str_replace(" ", "% %", $new_string);
+
+    return $new_string;
+
+
+
   }
 
 
