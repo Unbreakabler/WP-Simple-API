480e7203f1a4cdc668668cb60f0284d654f5ecd6
Jon Boyd <jonboyd21@gmail.com>
2016-04-13 12:39:02 -0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,31 +1,31 @@
 <?php
 
 /** MySQL database name **/
-define('SITE_URL', 'http://www.kamloopsthisweek.com');
+define('SITE_URL', '');
 
-define('DB_NAME', 'kamloops_wp');
+define('DB_NAME', '');
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', '');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '');
 
-define('TABLE_PREFIX', 'ez_');
+define('TABLE_PREFIX', '');
 
-//define the views meta_key in config, I doubt tie_views is standard
-define('VIEW_METAKEY', 'tie_views');
+//define the views meta_key in config
+define('VIEW_METAKEY', '');
 
-define('FEATURED_TERM_ID', '2');
-define('DEFAULT_TERM_ID', '5');
+define('FEATURED_TERM_ID', '');
+define('DEFAULT_TERM_ID', '');
 
 define('SECURE_TOKEN', '');
 
-define('DEFAULT_POST_IMAGE', 'http://www.kamloopsthisweek.com/wp-content/uploads/2014/09/NEWS-HEADER-2-300x150.jpg');
-define('DEFAULT_POST_IMAGE70', 'http://www.kamloopsthisweek.com/wp-content/uploads/2014/09/NEWS-HEADER-2-70x70.jpg');
-define('DEFAULT_POST_IMAGE150', 'http://www.kamloopsthisweek.com/wp-content/uploads/2014/09/NEWS-HEADER-2-150x150.jpg');
+define('DEFAULT_POST_IMAGE', '');
+define('DEFAULT_POST_IMAGE70', '');
+define('DEFAULT_POST_IMAGE150', '');
 
 ini_set('xdebug.var_display_max_depth', 5);
 ini_set('xdebug.var_display_max_children', 256);
